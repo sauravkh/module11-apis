6a513db421d4aafff9ff9e60616ba9fd8b9738b1
Saurav  Kharb <TheSauravKharb@D-173-250-155-166.dhcp4.washington.edu>
2017-01-31 15:17:16 -0800

    Add excercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,28 +1,46 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
+library("httr")
 
+library("jsonlite")
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
 
 # For what years does the API have statistical data?
+response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
+body <- content(response,"text")
+years <- fromJSON(body)
+years
 
 
 # What is the "country code" for the "Syrian Arab Republic"?
+response <- GET("http://data.unhcr.org/api/countries/list.json")
+countries <- fromJSON(content(response, "text"))
+
+filter(countries, name_en == "Syrian Arab Republic") %>%
+  select(country_code)
 
 
 # How many persons of concern from Syria applied for residence in the USA in 2013?
 # Hint: you'll need to use a query parameter
 # Use the `str()` function to print the data of interest
 # See http://www.unhcr.org/en-us/who-we-help.html for details on these terms
-
+query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", year = 2013)
+response <- GET("http://data.unhcr.org/api/stats/persons_of_concern.json", query = query.params)
+usa.person <- fromJSON(content(response,"text"))
+str(usa.person)
 
 ## And this was only 2013...
 
 
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
+query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", population_type_code = "RF")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json?", query = query.params)
+refugees.usa <- fromJSON(content(response,"text"))
+refugees.usa <- select(refugees.usa, year, usa = value)
 
 
 # Use the `plot()` function to plot the year vs. the value.
